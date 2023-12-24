9c5d8fc812588012649db1d553d712a0390debcb
Coronel4545 <stifler.oliveira41@gmail.com>
2023-12-23 22:15:49 -0300

    Add files via upload

---
+++ TravaPCjs/Trava.js
@@ -0,0 +1,12 @@
+
+
+
+function ForInLoop(){
+    
+
+        for(Valorinicial=0;Valorinicial<=100000000; Valorinicial ++){
+           
+            console.log(Valorinicial)
+        }
+        
+}
